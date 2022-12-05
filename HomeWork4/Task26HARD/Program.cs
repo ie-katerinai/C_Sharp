6fa46c9bae32ce869b2104ec09f3b62240fde17e
ie-katerinai <117760910+ie-katerinai@users.noreply.github.com>
2022-12-05 23:38:20 +0300

    Begining Task 26

--- HomeWork4/Task26HARD/Program.cs
+++ HomeWork4/Task26HARD/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 26. - HARD необязательная 
+// Напишите программу, которая принимает на вход целое или дробное число и выдаёт количество цифр в числе.
+// 452 -> 3
+// 82 -> 2
+// 9,012 ->4
+
+Console.WriteLine("Введите число "); // вывод запроса пользователю
+decimal N = Convert.ToDecimal(Console.ReadLine()); // конвертирование и запись значения в переменную
+
+decimal fraction = N % 1;
+int integer = Convert.ToInt32(N - fraction);
+
+Console.WriteLine($"Целая часть {integer} равна , дробная часть равна {fraction}");
