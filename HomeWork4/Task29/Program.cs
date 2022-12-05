1b5ea1703d7fff17a02216089004d32459d3dc64
ie-katerinai <117760910+ie-katerinai@users.noreply.github.com>
2022-12-05 21:30:09 +0300

    Begining Task 29

---
+++ HomeWork4/Task29/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8
+// элементов с клавиатуры и выводит массив на экран.
+
+int[] rendr(string arr) // функция создает массив
+{
+    int[] arr = new int[8]; // объявляем тип массива и даем ему имя, размерность
+    {
+        int i = 0;
+        string[] arrays = arr.Split(' ');
+        foreach (var arr in arrays)
+        {
+            arr[i] = Convert.ToInt32(arrays[i]);
+            i++;
+        }
+    }
+    return arr; // возвращает массив, так как тип функции int
+}
+
+array[] numb = s.Split(' ');
+
+foreach (var num in numbs)
+{
+    Console.WriteLine($"Substring: {sub}");
+}
