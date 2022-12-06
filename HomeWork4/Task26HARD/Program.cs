f65403d521e177cfe5d978c921738b3c1eb29f51
ie-katerinai <117760910+ie-katerinai@users.noreply.github.com>
2022-12-06 13:10:49 +0300

    Task 26 HARD - Done

--- HomeWork4/Task26HARD/Program.cs
+++ HomeWork4/Task26HARD/Program.cs
@@ -1,13 +1,50 @@
-﻿// Задача 26. - HARD необязательная 
+﻿// Задача 26. - HARD необязательная
 // Напишите программу, которая принимает на вход целое или дробное число и выдаёт количество цифр в числе.
 // 452 -> 3
 // 82 -> 2
 // 9,012 ->4
 
-Console.WriteLine("Введите число "); // вывод запроса пользователю
-decimal N = Convert.ToDecimal(Console.ReadLine()); // конвертирование и запись значения в переменную
+long ConvToInteger(decimal num1) // функция домнажает дробное число до целого
+{
+    decimal fraction = num1;
+    while (fraction != 0) // условие: пока дробная часть не будет равна 0
+    {
+        num1 = num1 * 10;
+        fraction = num1 % 1;
+    }
+    long integer = Convert.ToInt64(num1 - fraction); // переврдим в int, чтобы после запятой не писались нули, 64 - чтобы можно было ввести число больше 10 знаков
+    //Console.WriteLine($"Число дробное, увеличенное до целого {integer}");
+    return integer;
+}
 
-decimal fraction = N % 1;
-int integer = Convert.ToInt32(N - fraction);
+int CountingNumbers(long num2) // функция подсчета количества цифр в числе
+{
+    int count = 0; // обнуление счетчика
+    while (num2 != 0)
+    {
+        num2 = num2 / 10;
+        count++;
+    }
+    //Console.WriteLine($"Количество цифр {count}");
+    return count;
+}
 
-Console.WriteLine($"Целая часть {integer} равна , дробная часть равна {fraction}");
+try
+{
+    Console.WriteLine("Введите число "); // вывод запроса пользователю
+    decimal N = Convert.ToDecimal(Console.ReadLine()); // конвертирование и запись дробного значения в переменную
+    long integer = ConvToInteger(N);
+    Console.WriteLine($"количестыо цифр в числе: CountingNumbers(integer)");
+}
+catch (System.FormatException) // ищет неверный формат ввоа
+{
+    Console.WriteLine("Вы ввели не число");
+}
+
+// Console.WriteLine("Введите число "); // вывод запроса пользователю
+// decimal N = Convert.ToDecimal(Console.ReadLine()); // конвертирование и запись дробного значения в переменную
+
+// decimal fraction = N % 1; // отеление дробной части от целой
+// int integer = Convert.ToInt32(N - fraction); // отеление целой части от робной
+
+// Console.WriteLine($"Целая часть {integer} равна , дробная часть равна {fraction}");
