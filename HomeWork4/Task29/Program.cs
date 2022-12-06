4b84b43f9efc3900d71c5454d5eb3559fb977794
ie-katerinai <117760910+ie-katerinai@users.noreply.github.com>
2022-12-06 17:42:49 +0300

    Task 25

--- HomeWork4/Task29/Program.cs
+++ HomeWork4/Task29/Program.cs
@@ -1,24 +1,73 @@
 ﻿// Задача 29: Напишите программу, которая задаёт массив из 8
 // элементов с клавиатуры и выводит массив на экран.
 
-int[] rendr(string arr) // функция создает массив
+void Rendr(string text)
 {
-    int[] arr = new int[8]; // объявляем тип массива и даем ему имя, размерность
+    string[] stringArray = text.Split(' ');
+
+    if (stringArray.Length == 8)
     {
+        int[] intArray = new int[stringArray.Length];
         int i = 0;
-        string[] arrays = arr.Split(' ');
-        foreach (var arr in arrays)
+        foreach (var sub in stringArray)
         {
-            arr[i] = Convert.ToInt32(arrays[i]);
+            intArray[i] = Convert.ToInt32(sub);
+
+            Console.WriteLine(intArray[i]);
             i++;
         }
     }
-    return arr; // возвращает массив, так как тип функции int
+    else
+    {
+        Console.WriteLine("Вы ввели массив не из 8 элементов");
+    }
 }
-
-array[] numb = s.Split(' ');
-
-foreach (var num in numbs)
+try
 {
-    Console.WriteLine($"Substring: {sub}");
+    Console.WriteLine("Ввеите массив из 8 элементов через пробел");
+    string a = Console.ReadLine();
+    Rendr(a);
 }
+catch (System.Exception)
+{
+    Console.WriteLine("Введена некорректная строка");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int[] rendr(string arr) // функция создает массив
+// {
+//     int[] arr = new int[8]; // объявляем тип массива и даем ему имя, размерность
+//     {
+//         int i = 0;
+//         string[] arrays = arr.Split(' ');
+//         foreach (var arr in arrays)
+//         {
+//             arr[i] = Convert.ToInt32(arrays[i]);
+//             i++;
+//         }
+//     }
+//     return arr; // возвращает массив, так как тип функции int
+// }
+
+// array[] numb = s.Split(' ');
+
+// foreach (var num in numbs)
+// {
+//     Console.WriteLine($"Substring: {sub}");
+// }
