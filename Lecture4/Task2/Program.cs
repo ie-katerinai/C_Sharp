7d7eb0e9c3dad3009bb96b6ce0ad5f266bd2d377
ie-katerinai <117760910+ie-katerinai@users.noreply.github.com>
2022-12-06 21:36:04 +0300

    Add Lecture 4

---
+++ Lecture4/Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿
+void PrintArray(int[,] matr) // создает матрицу с 0
+{
+    for (int i = 0; i < matr.GetLength(0); i++) // ---> for (int i = 0; i < 3; i++ ) // щелкающие строки
+    {
+        for (int j = 0; j < matr.GetLength(1); j++) // ---->for (int j = 0; j < 4; j++) // щелкающие столбцы
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr) //заполняет матрицу случайными числами
+{
+    for (int i = 0; i < matr.GetLength(0); i++) // ---> for (int i = 0; i < 3; i++ ) // щелкающие строки
+    {
+        for (int j = 0; j < matr.GetLength(1); j++) // ---->for (int j = 0; j < 4; j++) // щелкающие столбцы
+        {
+            matr[i,j]= new Random().Next(1, 10);
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
