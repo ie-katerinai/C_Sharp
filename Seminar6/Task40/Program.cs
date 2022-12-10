a13a6a7d64b4d0e72d9080d0ad7e1b440a28324b
ie-katerinai <117760910+ie-katerinai@users.noreply.github.com>
2022-12-10 11:15:11 +0300

    Seminar6 Tasks 39, 40, 42

---
+++ Seminar6/Task40/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 40: Напишите программу, которая принимает на
+// вход три числа и проверяет, может ли существовать
+// треугольник с сторонами такой длины.
+
+void CreateArray(int[] mass) //заполняет массив
+{
+
+    for (int index = 0; index < mass.Length; index++)
+    {
+        mass[index] = Convert.ToInt32(Console.ReadLine());
+    }
+
+}
+
+void PrintArray(int[] mass)
+{
+    foreach (int el in mass)
+        Console.Write($"{el} ");
+    Console.WriteLine();
+}
+//if(num1<num2+num3) "Существует"
+//else "Не существует"
+
+void Check(int[] mass)
+{
+    int index = 0;
+    int sum = mass[index+1] + mass[index+2];
+    if(mass[index] < sum)  Console.WriteLine("Существует");
+    else Console.WriteLine("Не существует");
+}
+
+Console.WriteLine("Введите размеры строно треугольника");
+int[] array = new int[3];
+CreateArray(array);
+PrintArray(array);
+Check(array);
