cdf9d562de212f82b7e75b67904061e697b37ae3
ie-katerinai <117760910+ie-katerinai@users.noreply.github.com>
2022-12-12 18:08:39 +0300

    Seminar 6

--- Seminar6/Task44/Program.cs
+++ Seminar6/Task44/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи 0 и 1.
-Console.WriteLine("Введите, какое количество чисел Фибоначчи вывести на экран");
+/*Console.WriteLine("Введите, какое количество чисел Фибоначчи вывести на экран");
 int N = Convert.ToInt32(Console.ReadLine());
 int num1 = 0;
 int num2 = 1;
@@ -15,31 +15,33 @@ for (int i = 2; i <= N; i++)
 }
 
 
-/*
+*/
 
-void PrintArray(int[] array) // фунция вывода массива на экран
+void PrintArray(int[] mass) // фунция вывода массива на экран
 {
-    int count = array.Length;
-
-    for (int i=0; i < count; i++)
+    foreach (int el in mass)
     {
-        Console.Write($"{array[i]}");
+        Console.Write(el + " ");
     }
     Console.WriteLine();
 }
 
-void Fibonacci(int[] mass, int size)
+int[] Fibonacci(int size)
 {
-    mass[0] = 0;
-    mass[1] = 1;
-    for (int i = 2; i < size; i++)
-        mass[i] = mass[i - 2] + mass[i - 1];
+    int[] mass = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        if (i == 0)
+            mass[i] = 0;
+        else if (i == 1)
+            mass[i] = 1;
+        else
+            mass[i] = mass[i - 2] + mass[i - 1];
+    }
+    return mass;
 }
 
 Console.WriteLine("Введите, какое количество чисел Фибоначчи вывести на экран");
 int num = Convert.ToInt32(Console.ReadLine());
-int[] array = new int[num];
-//FillArray(array);
-
-Console.WriteLine($"{Fibonacci(array, num)}");
-PrintArray(array);*/
+int[] newArray = Fibonacci(num);
+PrintArray(newArray);
