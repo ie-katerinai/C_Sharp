7b69603edf6a29957d49fc3de1c23da540feaec1
ie-katerinai <117760910+ie-katerinai@users.noreply.github.com>
2022-12-16 00:29:29 +0300

    Add Task 1 HARD

--- HomeWork6/Task1HARD/Program.cs
+++ HomeWork6/Task1HARD/Program.cs
@@ -1,106 +1,68 @@
-﻿// Задача 1 HARD необязательная. 
-// Сгенерировать массив случайных целых чисел размерностью m*n (размерность вводим с клавиатуры). 
-// Вывести на экран красивенько таблицей. Найти минимальное число и его индекс, 
+﻿// Задача 1 HARD необязательная.
+// Сгенерировать массив случайных целых чисел размерностью m*n (размерность вводим с клавиатуры).
+// Вывести на экран красивенько таблицей. Найти минимальное число и его индекс,
 // найти максимальное число и его индекс. Вывести эту информацию на экран.
 
 void FillArray(int[,] array)
 {
-    for (int i=0;i<array.GetLength(0);i++)
-       for (int j=0;j<array.GetLength(1);j++) 
-            array[i,j] = new Random().Next(0,100);
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(0, 50);
 }
 
 void PrintArray(int[,] array)
 {
-    for (int i=0;i<array.GetLength(0);i++)
-       {
-        for (int j=0;j<array.GetLength(1);j++) 
-            Console.Write($"{array[i,j],3}\t");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j], 1}\t");
         Console.WriteLine();
-       }
+    }
 }
 
-void PrintStringArray(string[] array) // функция печати созданного массива
-{
-    foreach (string el in array)
-        Console.WriteLine($"{el} ");
-    Console.WriteLine();
-}
-
-int FindMin(int[,] array) // функция нахождения максимального элемента массива
-{
-    int min = array[0,0];
-    for (int i=0;i<array.GetLength(0);i++)
-       {
-        for (int j=0;j<array.GetLength(1);j++)
-            if (array[i, j] < min)
-            min = array[i, j];
-       }
-    return min;
-}
-
-int FindMax(int[,] array) // функция нахождения максимального элемента массива
+void FindMax(int[,] array) // функция поиска индекса максимального элемента массива
 {
     int max = array[0, 0];
-    for (int i=0;i<array.GetLength(0);i++)
-       {
-        for (int j=0;j<array.GetLength(1);j++)
-            if (array[i, j] > max)
-            max = array[i, j];
-       }
-    return max;
-}
-
-void FindIndexMax(int[,] array) // функция поиска индекса максимального элемента массива
-{
-    int max = array[0,0];
     int indexIMax = 0;
     int indexJMax = 0;
-    for (int i=0;i<array.GetLength(0);i++)
-       {
-        for (int j=0;j<array.GetLength(1);j++)
-        if (array[i,j] > max)
-        {
-            max = array[i,j];
-            indexIMax = i;
-            indexJMax = j;
-        }
-       }
-    Console.Write($"{indexIMax}, {indexJMax}");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            if (array[i, j] > max)
+            {
+                max = array[i, j];
+                indexIMax = i;
+                indexJMax = j;
+            }
+    }
+    Console.WriteLine($"Максимальный элемент равен {max}, его индекс:({indexIMax},{indexJMax})");
 }
 
-void FindIndexMin(int[,] array) // функция поиска индекса минимального элемента массива
+void FindMin(int[,] array) // функция поиска индекса минимального элемента массива
 {
-    int min = array[0,0];
+    int min = array[0, 0];
     int indexIMin = 0;
     int indexJMin = 0;
-    for (int i=0;i<array.GetLength(0);i++)
-       {
-        for (int j=0;j<array.GetLength(1);j++)
-        if (array[i, j] < min)
-        {
-            min = array[i, j];
-            indexIMin = i;
-            indexJMin = j;
-        }
-       }
-    Console.Write($"{indexIMin}, {indexJMin}");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            if (array[i, j] < min)
+            {
+                min = array[i, j];
+                indexIMin = i;
+                indexJMin = j;
+            }
+    }
+    Console.WriteLine($"Минимальный элемент равен {min}, его индекс:({indexIMin},{indexJMin})");
 }
-
-
 
 Console.WriteLine("Введите количество строк двумерного массива");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите количество столбцов двумерного массива");
 int n = Convert.ToInt32(Console.ReadLine());
-int[,] array = new int [m,n];
+int[,] array = new int[m, n];
 FillArray(array);
 PrintArray(array);
-
-string[] stringArray = new string[4];
-    stringArray[0] = "Минимальное число массива равно " + FindMin(array);
-    stringArray[1] = "Индекс минимального числа массива равен " + FindIndexMin(array);
-    stringArray[2] = "Максимальное число массива равно " + FindMax(array);
-    stringArray[3] = "Индекс максимального числа массива равен " + FindIndexMax(array);
-
-PrintStringArray(stringArray);
+Console.WriteLine();
+FindMin(array);
+FindMax(array);
