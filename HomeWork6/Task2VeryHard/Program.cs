80a1dae180fc38a2b1c84ce279646ec0dd995057
ie-katerinai <117760910+ie-katerinai@users.noreply.github.com>
2022-12-16 15:09:20 +0300

    To do

--- HomeWork6/Task2VeryHard/Program.cs
+++ HomeWork6/Task2VeryHard/Program.cs
@@ -1,11 +1,13 @@
-﻿/* Задача 2 VERY HARD необязательная 
-Имеется список чисел. Создайте список, в который попадают числа, описывающие максимальную возрастающую последовательность. 
+﻿/* Задача 2 VERY HARD необязательная
+Имеется список чисел. Создайте список, в который попадают числа, описывающие максимальную возрастающую последовательность.
 Порядок элементов менять нельзя.
 Одно число - это не последовательность.
 Пример:
 [1, 5, 2, 3, 4, 6, 1, 7] => [1, 7]
 [1, 5, 2, 3, 4, 1, 7, 8 , 15 , 1 ] => [1, 5]
 [1, 5, 3, 4, 1, 7, 8 , 15 , 1 ] => [3, 5]*/
+
+
 
 int[] FillArray(int size) // функция создания массива из случайных целых чисел
 {
@@ -37,7 +39,7 @@ void PrintArray(int[] array) // функция печати созданного
     Console.WriteLine();
 }
 
-int FindMin(int[] array) // функция нахождения максимального элемента массива
+int FindMin(int[] array) // функция нахождения минимальный элемента массива
 {
     int min = array[0];
     for (int i = 0; i < array.Length; i++)
@@ -55,64 +57,78 @@ int FindMax(int[] array) // функция нахождения максимал
     return max;
 }
 
-// int FindIndexMax(int[] array) // функция поиска индекса максимального элемента массива
-// {
-//     int max = array[0];
-//     int indexMax = 0;
-//     for (int i = 0; i < array.Length; i++)
-//         if (array[i] > max)
-//         {
-//             max = array[i];
-//             indexMax = i;
-//         }
-//     return indexMax;
-// }
-
-// int FindIndexMin(int[] array) // функция поиска индекса минимального элемента массива
-// {
-//     int min = array[0];
-//     int indexMin = 0;
-//     for (int i = 0; i < array.Length; i++)
-//         if (array[i] < min)
-//         {
-//             min = array[i];
-//             indexMin = i;
-//         }
-//     return indexMin;
-// }
-
-
-    // Console.WriteLine("Введите размерность массива");
-    // int size = Convert.ToInt32(Console.ReadLine());
-    int size =  new Random().Next(1, 10);
-    int[] array = FillArray(size);
-    Console.WriteLine("Исходный массив:");
-    PrintArray(array);
-    Console.WriteLine();
-    CheckUniqArray(array);
-    Console.WriteLine("Массив с уникальными элементами:");
-    PrintArray(array);
-    Console.WriteLine();
-    Console.WriteLine($"Минимальное число {FindMin(array)}");
-    int min = FindMin(array);
-    Console.WriteLine();
-    Console.WriteLine($"Максимальное число {FindMax(array)}");
-    int max = FindMax(array);   
-    CountNumb(array, min, max);
-    
-    
-int CountNumb (int[] array, int numMin, int numMax)
+string FindLength(int[] array, int number) // функция поиска длины последовательности
 {
-    int num = numMin;
-    int count = 0;
-    while (num <= max)
+    int[] massCount = new int[array.Length / 2];
+    string[] massNumbs = new string[array.Length / 2];
+    int max = 0;
+    for (int j = 0; j < array.Length - 1; j++)
     {
-        for (i=0; i < array.Length; i++)
+        
+        int count = 0;
+        for (int i = 0; i < array.Length - 1; i++)
         {
-            if  (num == array[i]) count++; num++;
-            else count = 0; num++;
+            if (number == array[i])
+            {
+                count++;
+                massNumbs[j] = ($"{array[i]}");
+                number++;
+            }
+            massCount[j] = count;
+        }
+        j = 0;
+        while (j < massCount.Length - 1)
+        {
+            max = massCount[j];
+            if (massCount[j + 1] > max)
+            {
+                max = massCount[j + 1];
+                j++;
+            }
+            else
+                j++;
         }
     }
-return count;
+    return massNumbs[max];
 }
-  
+
+int size = new Random().Next(1, 10);
+int[] array = FillArray(size);
+Console.WriteLine("Исходный массив:");
+PrintArray(array);
+Console.WriteLine();
+CheckUniqArray(array);
+Console.WriteLine("Массив с уникальными элементами:");
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine($"Минимальное число {FindMin(array)}");
+int min = FindMin(array);
+Console.WriteLine();
+Console.WriteLine($"Максимальное число {FindMax(array)}");
+int max = FindMax(array);
+Console.WriteLine();
+Console.WriteLine($"Количество элементов последовательности {FindLength(array, min)}");
+//CountNumb(array, min, max);
+
+// int CountNumb(int[] array, int numMin, int numMax)
+// {
+//     int num = numMin;
+//     int count = 0;
+//     while (num <= max)
+//     {
+//         for (int i = 0; i < array.Length; i++)
+//         {
+//             if (num == array[i])
+//             {
+//                 count++;
+//                 num++;
+//             }
+//             else
+//             {
+//                 count = 0;
+//                 num++;
+//             }
+//         }
+//     }
+//     return count;
+// }
