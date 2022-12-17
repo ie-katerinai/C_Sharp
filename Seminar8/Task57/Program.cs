90547446ae50a95d8506d71e62d0e162e996844f
ie-katerinai <117760910+ie-katerinai@users.noreply.github.com>
2022-12-17 11:56:09 +0300

    Seminar8

---
+++ Seminar8/Task57/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 57: Составить частотный словарь элементов двумерного массива. Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+
+// 1, 2, 3 
+// 4, 6, 1 
+// 2, 1, 6
+// 1 встречается 3 раза 
+// 2 встречается 2 раз 
+// 3 встречается 1 раз 
+// 4 встречается 1 раз 
+// 6 встречается 2 раза
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(0, 21);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j],3}\t");
+        Console.WriteLine();
+    }
+}
+
+void CountNumInArray(int[,] array)
+{
+    int[] tempArray = new int[21];
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            tempArray[array[i, j]] += 1;
+    for (int k = 0; k < 21; k++)
+    {
+        if (tempArray[k] != 0)
+            Console.WriteLine($"Число {k} встречается {tempArray[k]} раз");
+    }
+}
+
+Console.WriteLine("Ведите количество строк двумерного массива");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите количество столбцов двумерного массива");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, cols];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+CountNumInArray(array);
