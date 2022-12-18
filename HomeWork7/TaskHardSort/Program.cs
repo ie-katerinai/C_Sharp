29212e9441275707353123a9f6d6ff74e5a644d7
ie-katerinai <117760910+ie-katerinai@users.noreply.github.com>
2022-12-18 04:24:09 +0300

    Done

--- HomeWork7/TaskHardSort/Program.cs
+++ HomeWork7/TaskHardSort/Program.cs
@@ -1,5 +1,6 @@
 ﻿// Задача HARD SORT необязательная. Считается за три обязательных 
-// Задайте двумерный массив из целых чисел. Количество строк и столбцов задается с клавиатуры. Отсортировать элементы по возрастанию слева направо и сверху вниз.
+// Задайте двумерный массив из целых чисел. Количество строк и столбцов задается с клавиатуры. 
+// Отсортировать элементы по возрастанию слева направо и сверху вниз.
 // Например, задан массив:
 // 1 4 7 2
 // 5 9 10 3
@@ -7,14 +8,14 @@
 // 1 2 3 4
 // 5 7 9 10
 
-void FillArray(double[,] array)
+void FillArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
         for (int j = 0; j < array.GetLength(1); j++)
-            array[i, j] = new Random().Next(-20, 21);
+            array[i, j] = new Random().Next(0, 21);
 }
 
-void PrintArray(double[,] array)
+void PrintArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
@@ -24,13 +25,37 @@ void PrintArray(double[,] array)
     }
 }
 
-
+void SortArray(int[,] array)
+{
+    int buf = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(0); k++)
+            {
+                for (int l = 0; l < array.GetLength(1); l++)
+                {
+                    if(array[k, l] > array[i, j])
+                    {
+                        buf = array[k, l];
+                        array[k, l] = array[i, j];
+                        array[i, j] = buf;
+                    }
+                }
+            }
+        }
+    }
+}
 
 Console.WriteLine("Введите количество строк двумерного массива");
 int rows = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите количество столбцов двумерного массива");
 int cols = Convert.ToInt32(Console.ReadLine());
-double[,] array = new double[rows, cols];
+int[,] array = new int[rows, cols];
 FillArray(array);
-RendrArray(array);
+Console.WriteLine("Исходный массив:");
+PrintArray(array);
+Console.WriteLine("Отсортированный массив:");
+SortArray(array);
 PrintArray(array);
