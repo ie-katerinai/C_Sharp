72663178f030707a2a83a23b5fbc6d683fe8404f
ie-katerinai <117760910+ie-katerinai@users.noreply.github.com>
2022-12-24 02:30:24 +0300

    Add modified

--- HomeWork7/Task52/Program.cs
+++ HomeWork7/Task52/Program.cs
@@ -38,9 +38,28 @@ void CalcAverageSum(int[,] array)
     }
 }
 
+double[] CalcAvgSumArray(int[,] array)
+{
+    double sum = 0.0;
+    double[] result = new double[4];
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum = sum + Convert.ToDouble(array[i, j]);
+            result[i] = Math.Round((sum / array.GetLength(0)), 1);
+        }
+        
+        sum = 0.0;
+    }
+    return result;
+}
+
 int[,] array = new int[3, 4];
 FillArray(array);
 PrintArray(array);
 Console.WriteLine("Среднее арифметическое каждого столбца:");
 CalcAverageSum(array);
 Console.WriteLine();
+double[] mass = CalcAvgSumArray(array);
+Console.WriteLine(mass);
